ec0be6b0c68215b8b47e555214023c8aa1319e6e
Алексей Томилин <al.tomilin@bars-open.group>
2023-09-24 01:35:04 +0300

    orm1

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_order = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort_order is not None:
+        order = sort_order.replace('min_', '').replace('max_', '-')
+        phones = phones.order_by(order).values()
+    context = {
+        'phones': phones,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug).values()[0]
+    context = {
+        'phone': phone,
+    }
     return render(request, template, context)
