6826a5d159bd194a2eebb0735402dee87b011e39
Алексей Томилин <al.tomilin@bars-open.ru>
2023-10-03 18:16:14 +0300

    проверка на уникальность в форме

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Scope, Tag
 
 
+class ScopesInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        total_main_tags = 0
+        for form in self.forms:
+            total_main_tags += form.cleaned_data.get('is_main', 0)
+            if total_main_tags > 1:
+                raise ValidationError('Допустим только один основной тег')
+        return super().clean()
+
+
 class ScopesInline(admin.TabularInline):
     model = Scope
+    formset = ScopesInlineFormset
     extra = 1
 
 
